5e6ff1a48176e361e8da89e275847267418369ab
Massimocossi94 <massimocossi94@gmail.com>
2021-07-03 18:11:36 +0200

    make it better

--- controllers/books.js
+++ controllers/books.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 const Sequelize = require('sequelize');
 const Book = require('../models/book');
 
